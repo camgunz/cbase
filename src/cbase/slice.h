ae7f96159ae7c020c5c60598086a5ce321e3cd4a
Charlie Gunyon <charles.gunyon@gmail.com>
2017-08-12 23:20:20 -0400

    A little slice cleanup

--- src/cbase/slice.h
+++ src/cbase/slice.h
@@ -6,48 +6,34 @@ typedef struct {
     size_t len;
 } Slice;
 
-static inline void slice_assign(Slice *slice, char *data, size_t len) {
+static inline
+void slice_assign(Slice *slice, char *data, size_t len) {
     slice->data = data;
     slice->len = len;
 }
 
-static inline void slice_clear(Slice *slice) {
+static inline
+void slice_clear(Slice *slice) {
     slice->len = 0;
 }
 
-static inline bool slice_empty(Slice *slice) {
-    if (!slice->data) {
-        return true;
-    }
-
-    if (slice->len == 0) {
-        return true;
-    }
-
-    return false;
+static inline
+bool slice_empty(Slice *slice) {
+    return ((!slice->data) || (slice->len == 0));
 }
 
-static inline bool slice_equals_data_at_fast(Slice *slice, size_t index,
-                                                           const void *data,
+static inline
+bool slice_equals_data_at_fast(Slice *slice, size_t index, const void *data,
                                                            size_t len) {
     return memcmp(slice->data + index, data, len) == 0;
 }
 
-static inline bool slice_equals_data(Slice *slice, const char *data,
-                                                   size_t len) {
-    if (slice->len != len) {
-        return false;
-    }
-
-    return slice_equals_data_at_fast(slice, 0, data, len);
-}
-
-static inline bool slice_equals_data_at(Slice *slice, size_t index,
-                                                      const void *data,
+static inline
+bool slice_equals_data_at(Slice *slice, size_t index, const void *data,
                                                       size_t len,
                                                       bool *equal,
                                                       Status *status) {
-    if ((index + len) >= slice->len) {
+    if ((index + len) > slice->len) {
         return index_out_of_bounds(status);
     }
 
@@ -56,39 +42,53 @@ static inline bool slice_equals_data_at(Slice *slice, size_t index,
     return true;
 }
 
-static inline bool slice_equals(Slice *s1, Slice *s2) {
-    return slice_equals_data(s1, s2->data, s2->len);
-}
-
-static inline void slice_shallow_copy(Slice *dst, Slice *src) {
-    dst->data = src->data;
-    dst->len = src->len;
-}
-
-static inline bool slice_deep_copy(Slice *dst, Slice *src, Status *status) {
-    char *data = cbmalloc(src->len, sizeof(char));
-
-    if (!data) {
-        return alloc_failure(status);
+static inline
+bool slice_equals_data_fast(Slice *slice, const char *data, size_t len) {
+    if (slice->len != len) {
+        return false;
     }
 
-    cbmemmove(data, src->data, src->len);
+    return slice_equals_data_at_fast(slice, 0, data, len);
+}
 
-    dst->data = data;
-    dst->len = src->len;
+static inline
+bool slice_starts_with_data_fast(Slice *slice, const void *data, size_t len) {
+    return slice_equals_data_at_fast(slice, 0, data, len);
+}
+
+static inline
+bool slice_starts_with_data(Slice *slice, const void *data, size_t len,
+                                                            bool *equal,
+                                                            Status *status) {
+    if (len > slice->len) {
+        return index_out_of_bounds(status);
+    }
+
+    *equal = slice_starts_with_data_fast(slice, 0, data, len);
 
     return status_ok(status);
 }
 
-static inline void slice_read_fast(Slice *slice, size_t index, size_t len,
-                                                               void *out) {
+static inline
+bool slice_equals(Slice *s1, Slice *s2) {
+    return slice_equals_data(s1, s2->data, s2->len);
+}
+
+static inline
+void slice_shallow_copy(Slice *dst, Slice *src) {
+    dst->data = src->data;
+    dst->len = src->len;
+}
+
+static inline
+void slice_read_fast(Slice *slice, size_t index, size_t len, void *out) {
     cbmemmove(out, slice->data + index, len);
 }
 
-static inline bool slice_read(Slice *slice, size_t index, size_t len,
-                                                          void *out,
-                                                          Status *status) {
-    if ((index + len) >= slice->len) {
+static inline
+bool slice_read(Slice *slice, size_t index, size_t len, void *out,
+                                                        Status *status) {
+    if ((index + len) > slice->len) {
         return index_out_of_bounds(status);
     }
 
